2d8cf804175d594f1213ac7ec946ad257726acba
Nichita Utiu <nikita.utiu@gmail.com>
2018-07-06 22:52:25 +0300

    attempted fix for config vars

--- src/business/config.js
+++ src/business/config.js
@@ -1,5 +1,5 @@
 import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 // get url from env var(heroku) or use local
-export const HOST = runtimeEnv().API_URL !== undefined ? runtimeEnv().API_URL : 'http://localhost:8000' ;
+export const HOST = runtimeEnv().REACT_API_URL !== undefined ? runtimeEnv().API_URL : 'http://localhost:8000';
 export const API = '/api/v1';
