ac67d7659ac81b7b4627893a70c4ee8eb0522e37
Nichita Utiu <nikita.utiu@gmail.com>
2018-07-06 23:05:38 +0300

    3rd attempt

--- src/business/config.js
+++ src/business/config.js
@@ -1,3 +1,4 @@
+
 // get url from env var(heroku) or use local
-export const HOST = process.env.REACT_APP_SECRET_CODE !== undefined ? process.env.REACT_APP_SECRET_CODE : 'http://localhost:8000';
+export const HOST = process.env.REACT_APP_API_URL;
 export const API = '/api/v1';
